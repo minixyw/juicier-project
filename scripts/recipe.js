2f9d3281333d858271499ffc1b7d2c80332f5d71
MINIWU <MINIXYW@BU.EDU>
2022-05-10 00:19:34 -0400

    fixed

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -563,7 +563,7 @@ function findRecipes(data, choices){
     image.src = item.image;
     const link = document.createElement("a");
     link.href = "recipe.html?id=" + item.id;
-    link.textContent = 'link';
+    link.textContent = 'MORE';
     container.appendChild(card);
     card.appendChild(h2);
     card.appendChild(h3);
